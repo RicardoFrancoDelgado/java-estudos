8e4438e43cc90b9cbb775d82c9559bc66372d27d
Ricardo Franco Delgado <rf041555@gmail.com>
2024-05-21 20:25:35 -0300

    exercicio 10

---
+++ exercism-10/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números você vai digitar ?");
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um número: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        double maior = vect[0];
+        int posicaoMaior = 0;
+
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] > maior) {
+                maior = vect[i];
+                posicaoMaior = i;
+            }
+        }
+
+        System.out.printf("MAIOR VALOR = %.1f%n", maior);
+        System.out.printf("POSICAO DO MAIOR VALOR = %d\n", posicaoMaior);
+
+
+        sc.close();
+    }
+}
