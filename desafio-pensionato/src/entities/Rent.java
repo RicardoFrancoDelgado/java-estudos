bb618cd2fdcff3501d98700090772b0f3a48a831
Ricardo Franco Delgado <rf041555@gmail.com>
2024-05-23 17:13:20 -0300

    desafio pensionato

---
+++ desafio-pensionato/src/entities/Rent.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Rent {
+    private String name;
+    private String email;
+
+    public Rent() {
+    }
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return  name + ", " + email;
+    }
+}
