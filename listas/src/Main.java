c09418f3c93490d499c813d420e7e58ec97e56e9
Ricardo Franco Delgado <rf041555@gmail.com>
2024-06-03 15:04:22 -0300

    final da aula de listas

--- listas/src/Main.java
+++ listas/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,7 +28,20 @@ public class Main {
 
         System.out.println("--------------------------------");
 
+        System.out.println("Index of Bob: " + myList.indexOf("Bob"));
+        System.out.println("Index of Marco: " + myList.indexOf("Marco"));
 
+        System.out.println("--------------------------------");
+
+        List<String> result = myList.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String x : result) {
+            System.out.println(x);
+        }
+
+        System.out.println("--------------------------------");
+
+        String name = myList.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        System.out.println(name);
     }
 
 }
