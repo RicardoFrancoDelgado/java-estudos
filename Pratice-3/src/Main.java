556d288aaff592aed3d167073aeb83a132453c1b
Ricardo Franco Delgado <rf041555@gmail.com>
2024-06-07 00:33:28 -0300

    verificacao de posicao dos numeros adjacentes e verificacao de numero fora da matriz

--- Pratice-3/src/Main.java
+++ Pratice-3/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int m = sc.nextInt();
         int n = sc.nextInt();
 
+
         int[][] matriz = new int[m][n];
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
@@ -15,27 +16,41 @@ public class Main {
         }
 
         int x = sc.nextInt();
+        boolean found = false;
+
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] == x) {
+                    found = true;
                     System.out.println("Position: " + i + "," + j);
 
                     if (i > 0) {
-                        System.out.println("Up: " + matriz[i - 1][j]);
+                        System.out.println("Up: " + matriz[i - 1][j] + " | Position: " + (i - 1)+ "," + j);
+                    } else {
+                        System.out.println("No numbers above!");
                     }
                     if (j < matriz[i].length - 1) {
-                        System.out.println("Right: " + matriz[i][j + 1]);
+                        System.out.println("Right: " + matriz[i][j + 1] + " | Position: " + i + "," + (j + 1));
+                    } else {
+                        System.out.println("No numbers right!");
                     }
                     if (j > 0) {
-                        System.out.println("Left: " + matriz[i][j - 1]);
+                        System.out.println("Left: " + matriz[i][j - 1] + " | Position: " + i + "," + (j - 1));
+                    } else {
+                        System.out.println("No numbers left!");
                     }
                     if (i < matriz.length - 1) {
-                        System.out.println("Down: " + matriz[i + 1][j]);
+                        System.out.println("Down: " + matriz[i + 1][j] + " | Position: " + (i + 1)+ "," + j);
+                    } else {
+                        System.out.println("No numbers below!");
                     }
                 }
             }
         }
 
+        if(!found) {
+            System.out.println("The value " + x + " is not in the matriz.");
+        }
 
         sc.close();
     }
