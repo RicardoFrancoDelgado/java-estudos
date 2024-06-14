0c5cdcc7a60cf8d9295cb460b57cd90fbdd78011
Ricardo Franco Delgado <rf041555@gmail.com>
2024-06-13 21:38:08 -0300

    revisao matriz

---
+++ revision-matriz/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int linha = sc.nextInt();
+        int coluna = sc.nextInt();
+
+        int[][] matriz = new int[linha][coluna];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    System.out.println("Position: " + i + "," + j);
+                }
+            }
+        }
+
+
+        sc.close();
+    }
+}
